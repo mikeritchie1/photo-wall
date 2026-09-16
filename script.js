7893b8063994a67450fc8178984d99e8d3c3668f
Mike <mjritchie95@gmail.com>
2026-09-16 13:23:43 +0200

    Restore normal media frame visibility

--- script.js
+++ script.js
@@ -996,9 +996,6 @@ function handleUnavailableMedia(photo, imageData, requestId) {
   photo.videoEl.pause();
   photo.videoEl.removeAttribute("src");
   photo.videoEl.load();
-  photo.imgEl.style.opacity = "0";
-  photo.videoEl.style.opacity = "0";
-  photo.textEl.style.opacity = "0";
 
   resetImageCycle();
   if (imageCycle.length > 0) {
@@ -1232,9 +1229,6 @@ function assignRandomImageToPhoto(photo) {
     audioMinimumHoldUntil = 0;
   }
   photo.container.classList.remove("audio-active");
-  photo.imgEl.style.opacity = "0";
-  photo.videoEl.style.opacity = "0";
-  photo.textEl.style.opacity = "0";
   const nextCaption = getDisplayTextForImage(imageData);
   photo.audioStartPreparation?.();
   photo.currentMediaType = imageData.mediaType;
@@ -1254,8 +1248,6 @@ function assignRandomImageToPhoto(photo) {
       return;
     }
     photo.textEl.textContent = nextCaption;
-    photo.imgEl.style.opacity = "1";
-    photo.textEl.style.opacity = "1";
     photo.imgEl.onload = null;
     photo.imgEl.onerror = null;
   };
@@ -1269,8 +1261,6 @@ function assignRandomImageToPhoto(photo) {
       }
       photo.videoEl.onloadeddata = null;
       photo.videoEl.oncanplay = null;
-      photo.videoEl.style.opacity = "1";
-      photo.textEl.style.opacity = "1";
       finalizeCaptionUpdate();
     };
     photo.videoEl.onloadeddata = revealLoadedVideo;
